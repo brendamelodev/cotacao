0a62e46bc5cb142a5e61ff758ddde94c47c66e6b
brendamelodev <brendarebeka.melo@gmail.com>
2023-01-19 20:21:53 -0300

    Implementação de service para requisição e interface

--- cotacao-moeda/src/app/app.module.ts
+++ cotacao-moeda/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -16,7 +17,8 @@ import { FooterComponent } from './view/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ cotacao-moeda/src/app/model/moeda.ts
@@ -0,0 +1,4 @@
+export interface Moeda {
+  ID: string,
+  TEXT: string
+}
